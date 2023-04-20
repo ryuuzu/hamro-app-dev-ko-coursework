4a973e90312f2525a36f1ef6942d736d89f57012
ShreshaRosie <113813824+ShreshaRosie@users.noreply.github.com>
2023-04-20 12:14:51 +0545

    Added Interface for ApplicationDbContext and DateTime

---
+++ HKCRSystem.Application/Common/Interface/IApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HKCRSystem.Application.Common.Interface
+{
+    public interface IApplicationDBContext
+    {
+        //DbSet<Employee> Employee { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
