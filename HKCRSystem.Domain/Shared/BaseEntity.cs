b4c49451aa72225f6858c0f4426430295d18bf86
ShreshaRosie <113813824+ShreshaRosie@users.noreply.github.com>
2023-04-20 12:32:15 +0545

    Added BaseEntity

---
+++ HKCRSystem.Domain/Shared/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HKCRSystem.Domain.Shared
+{
+    public abstract class BaseEntity
+    {
+        public DateTime CreatedTime { get; set; }
+        public DateTime? UpdatedTime { get; set; }
+        public DateTime? DeletedTime { get; set; }
+        public int CreatedBy { get; set; }
+        public int UpdatedBy { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
