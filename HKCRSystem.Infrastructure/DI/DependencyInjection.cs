669cd92b4d76358aa9fdf1ea178c53de78e8749b
ShreshaRosie <113813824+ShreshaRosie@users.noreply.github.com>
2023-04-22 21:39:14 +0545

    Update for migrations

---
+++ HKCRSystem.Infrastructure/DI/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using HKCRSystem.Application.Common.Interface;
+using HKCRSystem.Infrastructure.Persistence;
+using HKCRSystem.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HKCRSystem.Infrastructure.DI
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDBContext>(
+                options =>
+                options.UseNpgsql(configuration.GetConnectionString("HKCRDatabase"),
+                b => b.MigrationsAssembly(typeof(ApplicationDBContext).Assembly.FullName)),
+                ServiceLifetime.Transient);
+            services.AddScoped<IApplicationDBContext>(provider =>
+                (IApplicationDBContext)provider.GetServices<ApplicationDBContext>()
+            );
+            // Add Services here.
+            services.AddTransient<IDateTime, DateTimeService>();
+            return services;
+        }
+    }
+}
