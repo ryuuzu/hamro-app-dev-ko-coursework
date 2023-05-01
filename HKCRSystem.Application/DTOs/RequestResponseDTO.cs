86d426cbfc761c164f82547011df039f1273933e
Utsav Gurmachhan <utsavmagar88728@gmail.com>
2023-05-01 20:29:55 +0545

    updated request response dto

--- HKCRSystem.Application/DTOs/RequestResponseDTO.cs
+++ HKCRSystem.Application/DTOs/RequestResponseDTO.cs
@@ -3,5 +3,13 @@
 public class RequestResponseDTO
 {
     public Guid Id { get; set; }
-    
+    public double Price { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public float Discount { get; set; }
+    public bool IsCancelled { get; set; }
+    public Guid RequestedCarId { get; set; }
+    public string RequestedById { get; set; }
+    public string ApprovedById { get; set; }
+    public Guid BillingId { get; set; }
 }
