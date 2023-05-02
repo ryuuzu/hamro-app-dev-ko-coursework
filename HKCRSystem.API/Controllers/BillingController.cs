90f2087840483037a2ea7f8bd447d05c622f1aa9
Avinna Maharjan <mhrznavinna401@gmail.com>
2023-05-02 21:09:01 +0545

    Add API to mark billing as paid.

--- HKCRSystem.API/Controllers/BillingController.cs
+++ HKCRSystem.API/Controllers/BillingController.cs
@@ -9,18 +9,31 @@ namespace HKCRSystem.API.Controllers;
 public class BillingController : ControllerBase
 {
     private readonly IBilling _billing;
+    private readonly IHelper _helper;
 
-    public BillingController(IBilling billing)
+    public BillingController(IBilling billing, IHelper helper)
     {
         _billing = billing;
+        _helper = helper;
     }
 
     [HttpGet]
-    [Authorize(Roles = "Admin")]
+    [Authorize(Roles = "Admin, Staff")]
     [Route("/api/user/get/billing")]
     public async Task<List<BillingResponseDTO>> GetAllBilling()
     {
         var result = await _billing.GetAllBilling();
+        return result;
+    }
+
+    [HttpPut]
+    [Authorize(Roles = "Admin, Staff")]
+    [Route("/api/update/billing/{id}")]
+    public async Task<ResponseDTO> UpdateBilling([FromRoute] Guid id)
+    {
+        //gets the id from the request header
+        string userId = _helper.GetIdFromToken(HttpContext);
+        var result = await _billing.UpdateBilling(id, userId);
         return result;
     }
 }
