012f6e2df6707b5ecf9668be531db181a4ab41b0
Avinna Maharjan <mhrznavinna401@gmail.com>
2023-05-03 08:51:34 +0545

    Utsav lai chaiyeko response.

---
+++ HKCRSystem.Blazor/Data/Services/LoginService.cs
@@ -0,0 +1,19 @@
+﻿using HKCRSystem.Blazor.Data.DTO;
+
+namespace HKCRSystem.Blazor.Data.Services
+{
+    public class LoginService
+    {
+        private readonly HttpClient _httpClient;
+        private string baseUrl = "https://localhost:7190/";
+        public LoginService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<ResponseDTO> UserLogin()
+        {
+
+        }
+    }
+}
