8b022cace21dcbb073c70463341ab44076602ec1
Avinna Maharjan <mhrznavinna401@gmail.com>
2023-05-03 10:18:36 +0545

    Request

--- HKCRSystem.Blazor/Data/Services/CarRequestService.cs
+++ HKCRSystem.Blazor/Data/Services/CarRequestService.cs
@@ -11,12 +11,49 @@ namespace HKCRSystem.Blazor.Data.Services
             _httpClient = httpClient;
         }
 
-        public async Task<ResponseDTO> RequestCar()
+        public async Task<ResponseDTO> RequestCar(string startDate, string endDate, string carId)
         {
             var request = new HttpRequestMessage(HttpMethod.Post, "https://localhost:7096/api/add/request");
             request.Headers.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJkY2ZmYzQ3MS0yOGIyLTRlODgtYWQwOC1kNDg2Yzk3OTkwZWQiLCJlbWFpbCI6ImhrY3JfYWRtaW5AZ21haWwuY29tIiwicm9sZSI6IkFkbWluIiwibmJmIjoxNjgzMDQxODc3LCJleHAiOjE2ODMwODUwNzcsImlhdCI6MTY4MzA0MTg3NywiaXNzIjoiaGtjcnMiLCJhdWQiOiJoa2Nycy1hcHAifQ.BgBHjhxg5IeaikHNbxKElvOr_K-bXdD_tvjPR8eyIuY");
-            var content = new StringContent("{\r\n  \"requestedCarId\": \"18393d86-59fd-4d81-a527-709f2a85112f\",\r\n  \"startDate\": \"2023-05-01T18:35:49.138Z\",\r\n  \"endDate\": \"2023-05-10T18:35:49.138Z\"\r\n}", null, "application/json");
+
+            var content = new MultipartFormDataContent();
+            content.Add(new StringContent(startDate), "endDate");
+            content.Add(new StringContent(endDate), "startDate");
+            content.Add(new StringContent(carId), "requestedCarId");
             request.Content = content;
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadAsStringAsync();
+            return new ResponseDTO { Status = "Success", Message = result };
+        }
+
+        public async Task<ResponseDTO> CancelRequest(string requestId)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, $"https://localhost:7096/api/cancel/request/{requestId}");
+            request.Headers.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJkY2ZmYzQ3MS0yOGIyLTRlODgtYWQwOC1kNDg2Yzk3OTkwZWQiLCJlbWFpbCI6ImhrY3JfYWRtaW5AZ21haWwuY29tIiwicm9sZSI6IkFkbWluIiwibmJmIjoxNjgzMDQxODc3LCJleHAiOjE2ODMwODUwNzcsImlhdCI6MTY4MzA0MTg3NywiaXNzIjoiaGtjcnMiLCJhdWQiOiJoa2Nycy1hcHAifQ.BgBHjhxg5IeaikHNbxKElvOr_K-bXdD_tvjPR8eyIuY");
+
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadAsStringAsync();
+            return new ResponseDTO { Status = "Success", Message = result };
+        }
+
+        public async Task<ResponseDTO> ApproveRequest(string requestId)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, $"https://localhost:7096/api/accept/request/{requestId}");
+            request.Headers.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJkY2ZmYzQ3MS0yOGIyLTRlODgtYWQwOC1kNDg2Yzk3OTkwZWQiLCJlbWFpbCI6ImhrY3JfYWRtaW5AZ21haWwuY29tIiwicm9sZSI6IkFkbWluIiwibmJmIjoxNjgzMDQxODc3LCJleHAiOjE2ODMwODUwNzcsImlhdCI6MTY4MzA0MTg3NywiaXNzIjoiaGtjcnMiLCJhdWQiOiJoa2Nycy1hcHAifQ.BgBHjhxg5IeaikHNbxKElvOr_K-bXdD_tvjPR8eyIuY");
+
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadAsStringAsync();
+            return new ResponseDTO { Status = "Success", Message = result };
+        }
+
+        public async Task<ResponseDTO> DenyRequest(string requestId)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, $"https://localhost:7096/api/deny/request/{requestId}");
+            request.Headers.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJkY2ZmYzQ3MS0yOGIyLTRlODgtYWQwOC1kNDg2Yzk3OTkwZWQiLCJlbWFpbCI6ImhrY3JfYWRtaW5AZ21haWwuY29tIiwicm9sZSI6IkFkbWluIiwibmJmIjoxNjgzMDQxODc3LCJleHAiOjE2ODMwODUwNzcsImlhdCI6MTY4MzA0MTg3NywiaXNzIjoiaGtjcnMiLCJhdWQiOiJoa2Nycy1hcHAifQ.BgBHjhxg5IeaikHNbxKElvOr_K-bXdD_tvjPR8eyIuY");
+
             var response = await _httpClient.SendAsync(request);
             response.EnsureSuccessStatusCode();
             var result = await response.Content.ReadAsStringAsync();
--- HKCRSystem.Blazor/Program.cs
+++ HKCRSystem.Blazor/Program.cs
@@ -15,6 +15,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
 builder.Services.AddScoped<LoginService>();
+builder.Services.AddScoped<CarRequestService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredLocalStorage();
