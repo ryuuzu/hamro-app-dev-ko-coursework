ddd6f6793a892b027547d89e6e5530ba04cebb57
Avinna Maharjan <mhrznavinna401@gmail.com>
2023-05-03 10:33:53 +0545

    Payment

---
+++ HKCRSystem.Blazor/Data/Services/PaymentService.cs
@@ -0,0 +1,26 @@
+﻿using HKCRSystem.Blazor.Data.DTO;
+
+namespace HKCRSystem.Blazor.Data.Services
+{
+    public class PaymentService
+    {
+
+        private readonly HttpClient _httpClient;
+
+        public PaymentService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<ResponseDTO> CreatePayment(string billingId)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, $"https://localhost:7096/api/payment/{billingId}");
+            request.Headers.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJkY2ZmYzQ3MS0yOGIyLTRlODgtYWQwOC1kNDg2Yzk3OTkwZWQiLCJlbWFpbCI6ImhrY3JfYWRtaW5AZ21haWwuY29tIiwicm9sZSI6IkFkbWluIiwibmJmIjoxNjgzMDQxODc3LCJleHAiOjE2ODMwODUwNzcsImlhdCI6MTY4MzA0MTg3NywiaXNzIjoiaGtjcnMiLCJhdWQiOiJoa2Nycy1hcHAifQ.BgBHjhxg5IeaikHNbxKElvOr_K-bXdD_tvjPR8eyIuY");
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadAsStringAsync();
+
+            return new ResponseDTO { Status = "Success", Message = result };
+        }
+    }
+}
