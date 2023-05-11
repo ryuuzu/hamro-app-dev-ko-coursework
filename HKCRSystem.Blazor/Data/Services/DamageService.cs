596fcbd5533b818ac21d8381218b97a85c7c838c
Avinna Maharjan <mhrznavinna401@gmail.com>
2023-05-12 00:25:50 +0545

    Damage office

--- HKCRSystem.Blazor/Data/Services/DamageService.cs
+++ HKCRSystem.Blazor/Data/Services/DamageService.cs
@@ -1,13 +1,16 @@
 ﻿using HKCRSystem.Blazor.Data.DTO;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http.Headers;
+using System.Net;
+using System.Data;
+using System.Text;
 
 namespace HKCRSystem.Blazor.Data.Services
 {
     public class DamageService
     {
         private readonly HttpClient _httpClient;
-
-        private string baseUrl = "https://localhost:7284/";
 
         public DamageService(HttpClient httpClient)
         {
@@ -31,28 +34,42 @@ namespace HKCRSystem.Blazor.Data.Services
             return new ResponseDTO { Status = "Success", Message = result };
         }
 
-        public async Task<List<DamageData>?> GetDamageAsync()
+        public async Task<List<DamageData>?> GetDamageAsync(string token)
         {
-            var response = await _httpClient.GetAsync("https://localhost:7284/api/get/damage");
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:7096/api/get/damage");
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
-            var result = response.Content.ReadAsStringAsync().Result;
+            var response = await _httpClient.SendAsync(request);
+            var result = await response.Content.ReadAsStringAsync();
+
+            if (response.StatusCode == HttpStatusCode.Forbidden)
+            {
+                return null;
+            }
+
             var rr = JsonConvert.DeserializeObject<List<DamageData>>(result);
             return rr;
         }
 
-        public async Task<ResponseDTO> UpdateDamage(string damageId, string price, string token)
+        public async Task<ResponseDTO> UpdateDamage(string damageId, double price, string token)
         {
-            var request =
-                new HttpRequestMessage(HttpMethod.Put, $"https://localhost:7096/api/update/damage/{damageId}");
+            var request = new HttpRequestMessage(HttpMethod.Put, $"https://localhost:7096/api/update/damage/{damageId}");
             request.Headers.Add("Authorization", $"Bearer {token}");
-            var content = new MultipartFormDataContent();
-            content.Add(new StringContent(price), "price");
-            request.Content = content;
-            var response = await _httpClient.SendAsync(request);
-            response.EnsureSuccessStatusCode();
-            var result = await response.Content.ReadAsStringAsync();
+            var data = new
+            {
+                Price = price
+            }; 
+            var json = JsonConvert.SerializeObject(data);
+            request.Content = new StringContent(json, Encoding.UTF8, "application/json");
 
-            return new ResponseDTO { Status = "Success", Message = result };
+            var response = await _httpClient.SendAsync(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.Forbidden)
+            {
+                return null;
+            }
+            var result = await response.Content.ReadAsStringAsync();
+            var resulta = JsonConvert.DeserializeObject<ResponseDTO>(result);
+            return resulta;
         }
     }
 }
